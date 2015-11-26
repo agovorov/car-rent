2cf67a4109fcd7d6a2847033dbdff7afcfb46c34
Andy Govorov <thegovorovs@gmail.com>
2015-11-26 15:41:14 +0600

    New entities: order, user

---
+++ src/main/java/com/epam/ag/model/Order.java
@@ -0,0 +1,7 @@
+package com.epam.ag.model;
+
+/**
+ * @author Govorov Andrey
+ */
+public class Order extends BaseEntity {
+}
---
+++ src/main/java/com/epam/ag/model/User.java
@@ -0,0 +1,7 @@
+package com.epam.ag.model;
+
+/**
+ * @author Govorov Andrey
+ */
+public class User extends BaseEntity {
+}
